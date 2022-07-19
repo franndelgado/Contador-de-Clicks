89428904f58fd59f82146dea5855451b3a231819
Francisca Delgado <delgadofrancisca22@gmail.com>
2022-07-19 10:08:46 -0300

    all done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Button from './components/Button';
+import Counter from './components/Counter';
 
 function App() {
+
+  const [numClicks, setNumClicks] = useState(0);
+
+  const handleClick = () => {
+    setNumClicks(numClicks + 1);
+  };
+
+  const resetCounter = () => {
+    setNumClicks(0);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title" >
+        <h1>Contador de Clicks</h1>
+      </div>
+      <div className="main-container">
+        <Counter
+          numClicks={numClicks}/>
+
+        <Button
+          text="Click"
+          isClickButton={true}
+          handleClick={handleClick} />
+        <Button
+          text="Restart"
+          isClickButton={false}
+          handleClick={resetCounter} />
+      </div>
     </div>
   );
 }
